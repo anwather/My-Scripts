c3e5faab3e99b0504905a9d3789423254e1be18a
Anthony Watherston <anwather@microsoft.com>
2016-06-14 11:29:29 +1000

    Initial commit

---
+++ Get-Blob.ps1
@@ -0,0 +1,10 @@
+﻿Function Get-Blob
+    {
+    Param($ResourceGroupName,$StorageAccountName,$StorageContainerName,$File,$Destination)
+
+    $context = Get-AzureRmStorageAccount -ResourceGroupName $ResourceGroupName -Name $StorageAccountName -Verbose
+
+    $blob = Get-AzureStorageBlob -Container $StorageContainerName -Context $context -Verbose
+
+    $blob | Get-AzureStorageBlobContent -Destination $Destination -Verbose
+    }
