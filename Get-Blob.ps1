d4d406becc5c90d7853c7d72205771090879402d
Anthony Watherston <anwather@microsoft.com>
2016-06-17 13:23:12 +1000

    Added switch for grid display

--- Get-Blob.ps1
+++ Get-Blob.ps1
@@ -4,20 +4,37 @@
     [CmdletBinding()]
 
     Param(
+        [Parameter(Mandatory=$true)]
         [string]$ResourceGroupName,
+        
+        [Parameter(Mandatory=$true)]
         [string]$StorageAccountName,
         
+        [Parameter(Mandatory=$true)]
         [ValidateScript({$_ -match "^[a-z\-]*$"})]
         [string]$StorageContainerName,
+        
+        [Parameter(ParameterSetName="Set1")]
         [string]$File,
 
+        [Parameter(Mandatory=$true)]
         [ValidateScript({Test-Path $_})]
-        [string]$Destination
+        [string]$Destination,
+
+        [Parameter(ParameterSetName="Set2")]
+        [switch]$Grid
          )
 
     $context = (Get-AzureRmStorageAccount -ResourceGroupName $ResourceGroupName -Name $StorageAccountName -Verbose).Context
 
-    $blob = Get-AzureStorageBlob -Container $StorageContainerName -Context $context | Where Name -eq $File
-
+    if ($Grid)
+        {
+            $blob = Get-AzureStorageBlob -Container $StorageContainerName -Context $context | Out-GridView -PassThru
+        }
+    else
+        {
+            $blob = Get-AzureStorageBlob -Container $StorageContainerName -Context $context | Where Name -eq $File
+        }
+    
     $blob | Get-AzureStorageBlobContent -Destination $Destination -Verbose
     }
