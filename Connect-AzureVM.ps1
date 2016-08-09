d43f5a390cd22f94ccd7a9cd946533ce5bebfb4f
Anthony Watherston <anwather@microsoft.com>
2016-08-10 09:38:24 +1000

    Added file Connect-AzureVM.ps1

---
+++ Connect-AzureVM.ps1
@@ -0,0 +1,16 @@
+﻿Function Connect-AzureVM
+    {
+
+    [CmdletBinding()]
+    Param(
+    $ResourceGroupName,
+    $ComputerName
+    )
+
+    $ipAddress = (Get-AzureRmPublicIpAddress -Name $computerName -ResourceGroupName $resourceGroupName).IpAddress
+    
+    $cmd = "mstsc /v:$ipaddress`:3389"
+
+    Invoke-Expression $cmd
+
+    }
