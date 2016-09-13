50c1d20c3c253909364b5393311ebce69926ac3b
Anthony Watherston <anwather@users.noreply.github.com>
2016-09-13 15:55:41 +1000

    Add files via upload

---
+++ Get-ServiceInfoPipelineExample.ps1
@@ -0,0 +1,20 @@
+﻿Function Get-ServiceInfo
+    {
+
+    [CmdletBinding()]
+    Param(
+
+    [Parameter(Mandatory=$true)]
+    [string]$Name,
+
+    [Parameter(ValueFromPipeline=$true)]
+    [string[]]$ComputerName="localhost"
+    )
+
+    Process 
+        {
+            Write-Verbose "Checking service: $Name on computer: $_"
+            Get-Service -Name $Name -ComputerName $_
+        }
+    
+    }
