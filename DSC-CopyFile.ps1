909e42a81507ae582361d3664b64ba45858e6126
Anthony Watherston <anwather@users.noreply.github.com>
2016-09-15 14:16:14 +1000

    Add files via upload

---
+++ DSC-CopyFile.ps1
@@ -0,0 +1,18 @@
+﻿Configuration CopyFile
+    {
+
+    Import-DSCResource -ModuleName xRoboCopy,PSDesiredStateConfiguration
+
+    Node localhost
+        {
+
+        xRoboCopy CopyFiles
+            {
+                Source="C:\Temp\Folder1"
+                Destination="C:\Temp\Folder2"
+                AdditionalArgs = "/MIR"
+            }
+
+        }
+    }
+
