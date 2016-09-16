14c40e5b2d6ac5130344756300b0aab59fa87cc0
Anthony Watherston <anwather@users.noreply.github.com>
2016-09-16 10:35:25 +1000

    Add files via upload

---
+++ ArrayList.ps1
@@ -0,0 +1,24 @@
+﻿$serverlist = "DC01","MS01","SVR01"
+$arrayList = New-Object System.Collections.ArrayList
+foreach ($server in $serverlist){$arrayList.Add($server)}
+$orderedArray = @()
+[boolean]$end = $false
+
+do
+{
+
+if ($arrayList[-1] -notmatch "DC")
+    {
+    Write-output $arrayList[-1]
+    $orderedArray += $arrayList[-1]
+    $arrayList.Remove($arrayList[-1])
+    }
+if ($arrayList.Count -eq 1)
+    {
+    $orderedArray += $arrayList[0]
+    $end = $true
+    }
+
+    
+}
+until ($end -eq $true)
