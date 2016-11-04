7c7afdd04b633498db2aa974aad3b0135dd00636
Anthony Watherston <anwather@microsoft.com>
2016-11-04 13:25:59 +1100

    added ftp script :qw

---
+++ FTP-ConnectExamples.ps1
@@ -0,0 +1,10 @@
+﻿$ftploc = "ftp://waws-prod-ch1-005.ftp.azurewebsites.windows.net"
+#$cred = Get-Credential
+[System.Net.FtpWebRequest]$Request = [System.Net.WebRequest]::Create($ftploc)
+
+$Request.Credentials = $cred
+$Request.Method = [System.Net.WebRequestMethods+FTP]::ListDirectoryDetails
+
+[System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$ignoreCert}
+$Response = $Request.GetResponse()
+$Response.Close()
