02457dccb457436fe318371daeca137ca0411171
Anthony Watherston <anwather@microsoft.com>
2017-03-10 11:11:08 +1100

    new file

---
+++ PipeLineDemo.ps1
@@ -0,0 +1,13 @@
+﻿Function PipeLineDemo
+{
+    [CmdletBinding()]
+    Param(
+    [Parameter(ValueFromPipeline=$true)]
+    [string[]]$Input1
+    )
+
+    Process {
+    Write-Output $Input1
+    }
+
+}
