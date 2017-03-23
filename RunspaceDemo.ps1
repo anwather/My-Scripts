512f787bdcc42cb42500121a4e0f5e29b6e12ee8
Anthony Watherston <anwather@microsoft.com>
2017-03-23 16:44:57 +1100

    Added runspace demo

---
+++ RunspaceDemo.ps1
@@ -0,0 +1,26 @@
+﻿$jn = 1..10
+$jobs = @()
+
+$scriptblock = {
+    $file = New-TemporaryFile
+    return $file.Name
+    }
+
+$runSpacePool = [runspacefactory]::CreateRunspacePool(1,10).Open()
+
+foreach ($j in $jn)
+{
+    $job = [powershell]::Create().AddScript($scriptblock)
+    $job.RunspacePool = $runSpacePool
+    
+    $jobs += [pscustomobject]@{
+        Pipe = $job
+        Result = $job.BeginInvoke()
+    }
+}
+
+$jobs | ForEach-Object {
+
+        $results += $_.Pipe.EndInvoke($_.Result)
+
+    }
