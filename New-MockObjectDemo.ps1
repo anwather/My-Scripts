6418724c183acca800f9c26dfd44b7026214b47a
Anthony Watherston <anwather@microsoft.com>
2017-06-06 16:13:37 +0800

    Added new-mock object

---
+++ New-MockObjectDemo.ps1
@@ -0,0 +1,13 @@
+﻿Describe "Mock-Object Tests" {
+
+    It "Creates a new mock object" {
+        mock 'Get-ChildItem' { 
+            
+            $obj = New-MockObject -Type 'System.IO.FileInfo' -Verbose -Debug
+           Grandma
+        }
+
+        Get-ChildItem c:\Windows
+        Assert-MockCalled -CommandName Get-ChildItem -Times 1
+    }
+}
