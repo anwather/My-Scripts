6466131277665b65eb57e3e6b13aeb55261d4952
Anthony Watherston <anwather@microsoft.com>
2017-06-07 12:37:00 +0800

    updated splatting demo

--- Splatting-Example.ps1
+++ Splatting-Example.ps1
@@ -1,6 +1,11 @@
-﻿function list
-    {
-    param([HashTable]$properties)
-            
-    Get-ChildItem @properties | Format-Wide
-    }
+﻿$paths = "C:\Source","C:\AMD","C:\Intel"
+
+foreach ($path in $paths) {
+$params = @{
+    Path = $path
+    Recurse = $true
+    Depth = 0
+}
+
+Get-ChildItem @params 
+}
